10c07a5d021d183d3a8d5cff5bb19a4e637ab413
PixelStudios138 <marleybroadbent@gmail.com>
2025-03-28 12:09:55 +1100

    Create drivingtest.py
    Created a test case for driving and turning

---
+++ drivingtest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+ev3 = EV3Brick()
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+robot.straight(1000)
+ev3.speaker.beep()
+
+robot.straight(-1000)
+ev3.speaker.beep()
+
+robot.turn(90)
+ev3.speaker.beep()
+
+robot.turn(-90)
+ev3.speaker.beep()
