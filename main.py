2492f2be94b0fe3288622e48150fef8b721fbd41
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-04-04 12:50:21 +1100

    4/4/25

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env pybricks-micropython
+
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, GyroSensor, Colorsensor
+from pybricks.ev3devices import Motor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 
@@ -7,20 +9,13 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-gyro = GyroSensor(Port.S2)
 
 turn_angles = []
 
 def turn_by_angle(angle): # function for allowing robot to turn around
-    gyro.reset_angle(0)
-    if angle > 0:  # clockwise 
-        while gyro.angle() < angle:
-            robot.drive(0, 50)
-    else:  # counterclockwise
-        while gyro.angle() > angle:
-            robot.drive(0, -50)
+    robot.turn(angle)
     robot.stop()
-    turn_angles.append(angle)  # Record the turn
+    turn_angles.append(angle)  # Record the turns
 
 def navigate_course():
     if Color.RED: # if block is red, it will return to start of course position
