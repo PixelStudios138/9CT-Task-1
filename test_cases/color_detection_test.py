6b3ffc0d8ca9d1aa2038905d4fff0626e0094251
Oscar Coleman <161176613+oscarcoleman@users.noreply.github.com>
2025-05-02 13:28:12 +1000

    2/5

--- test_cases/color_detection_test.py
+++ test_cases/color_detection_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import Motor, ColorSensor, GyroSensor
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
@@ -10,8 +10,13 @@ ev3 = EV3Brick()
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+<<<<<<< Updated upstream
 color_sensor1 = ColorSensor(Port.S4)
 color_sensor2 = ColorSensor(Port.S3)
+=======
+color_sensor = ColorSensor(Port.S3)
+gyro = GyroSensor(Port.S2)
+>>>>>>> Stashed changes
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
@@ -23,16 +28,29 @@ def get_color1():
         return None  
     return detected_color
 
+<<<<<<< Updated upstream
 def get_color2():
     detected_color = color_sensor2.color()
     if detected_color is None:
         return None
     return detected_color
+=======
+<<<<<<< HEAD
+=======
+def turn_by_angle(angle): 
+    robot.turn(angle)
+    robot.straight(500)
+    robot.stop()
+    turn_angles.append(angle)
+    navigate_course()
+>>>>>>> Stashed changes
 
+>>>>>>> 046b91db33ede9df79d2a21b51a860e8deda8bd3
 def navigate_course():
     detected_color1 = get_color1()
     detected_color2 = get_color2()
 
+<<<<<<< Updated upstream
     if detected_color1 in [Color.RED, Color.YELLOW]:
         while detected_color2 not in [Color.BLACK]:
             robot.straight()
@@ -40,7 +58,17 @@ def navigate_course():
     else:
         robot.turn(90)
         robot.straight(500)
+=======
+    if detected_color in [Color.RED, Color.YELLOW]:
+        # while black line has not been crossed, go forward.
+        # when it's crossed, move forward 10cm
+    else:
+<<<<<<< HEAD
+=======
+        turn_by_angle(90)
+>>>>>>> Stashed changes
 
+>>>>>>> 046b91db33ede9df79d2a21b51a860e8deda8bd3
 
 while True:
     navigate_course()
